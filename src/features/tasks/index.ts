36c3075b9ab843e54772f6f30595fccfda02d852
VitaliiBasalai <57222348+Bulnaug@users.noreply.github.com>
2026-05-03 19:37:06 +0200

    Fix missing exports for tasks pages

--- src/features/tasks/index.ts
+++ src/features/tasks/index.ts
@@ -1,2 +1,4 @@
 export { TasksLayout } from "./layout/TasksLayout";
 export { default as TasksPage } from "./pages/TasksPage";
+export { default as NewTaskPage } from "./pages/NewTaskPage";
+export { default as TasksCalendarPage } from "./pages/TasksCalendarPage";
